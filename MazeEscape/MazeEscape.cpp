0fa315252d200928016290b7b17eb35435956957
Marian Pekar <marian.pekar@gmail.com>
2024-06-13 01:02:58 +0200

    Implement game loop

--- MazeEscape/MazeEscape.cpp
+++ MazeEscape/MazeEscape.cpp
@@ -11,21 +11,39 @@ int main(int argc, char* argv[])
 
     Console console;
 
-    Maze maze(width, height);
-    maze.Draw(console);
-
-    Actor player(1, 1, '@', Blue);
-    player.Draw(console);
-
-    Actor baba(width - 3, 1, 'B', Red);
-    baba.Draw(console);
-
-    while (true)
+    int scorePlayer = 0;
+    int scoreBaba = 0;
+    
+    while(true)
     {
-        PlayerController::GetInput(console, player, maze);    
+        Maze maze(width, height);
+        maze.Draw(console);
+
+        Actor player(1, 1, '@', Blue);
         player.Draw(console);
-        
-        AgentController::Move(player.GetX(), player.GetY(), baba, maze);
+
+        Actor baba(width - 3, 1, 'B', Red);
         baba.Draw(console);
+
+        while (true)
+        {
+            if (player.GetX() == baba.GetX() && player.GetY() == baba.GetY())
+            {
+                scoreBaba++;
+                break;
+            }
+
+            if (player.GetY() == height - 2)
+            {
+                scorePlayer++;
+                break;
+            }
+            
+            PlayerController::GetInput(console, player, maze);    
+            player.Draw(console);
+        
+            AgentController::Move(player.GetX(), player.GetY(), baba, maze);
+            baba.Draw(console);
+        }
     }
 }
